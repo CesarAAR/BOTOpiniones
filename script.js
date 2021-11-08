0e6cb381e87eedbd81877eaa95b3d0e98fbc5fc3
CesarAAR <cealalvarezr@ittepic.edu.mx>
2021-11-08 09:57:11 -0700

    quite comillas al input y output

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ function iniciar(){
 
     const datos = [
         {
-            "input":"Excelente labor... Una Ciudad con luz, será más segura... Bendiciones",
-            "output":{"Positivo":1}
+            input:"Excelente labor... Una Ciudad con luz, será más segura... Bendiciones",
+            output:{"Positivo":1}
         },{
-            "input":"El recorrido debería ser más fluido. Demasiada gente",
-            "output":{"Negativo":1}  
+            input:"El recorrido debería ser más fluido. Demasiada gente",
+            output:{"Negativo":1}  
         },{
-            "input":"Todo les quedo hermoso",
-            "output":{"Positivo":1}  
+            input:"Todo les quedo hermoso",
+            output:{"Positivo":1}  
         },{
-            "input":"Excelente horario, si bien es cierto agrada tu forma de decisiones y trabajo.",
-            "output":{"Positivo":1}  
+            input:"Excelente horario, si bien es cierto agrada tu forma de decisiones y trabajo.",
+            output:{"Positivo":1}  
         },{
-            "input":"En su cabeza sienten que somos de primer mundo cuando realmente es nefasto este señor",
-            "output":{"Negativo":1}  
+            input:"En su cabeza sienten que somos de primer mundo cuando realmente es nefasto este señor",
+            output:{"Negativo":1}  
         },{
-            "input":"Se ven muy contentos",
-            "output":{"Positivo":1}  
+            input:"Se ven muy contentos",
+            output:{"Positivo":1}  
         }
     ];
 
